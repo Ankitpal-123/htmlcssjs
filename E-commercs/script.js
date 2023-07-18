9d17b96a74184e1dd6d5a6a4cf9b8a2cd8e97687
rpinfoc <79321761+Ankitpal-123@users.noreply.github.com>
2023-07-18 15:31:11 +0530

    E-commerce completed

--- E-commercs/script.js
+++ E-commercs/script.js
@@ -14,3 +14,20 @@ if (close) {
     nav.classList.remove("active");
   });
 }
+
+// sproduct details
+const mainimg = document.getElementById("mainimg");
+const smallimg = document.getElementsByClassName("small-img");
+
+smallimg[0].onclick = function () {
+  mainimg.src = smallimg[0].src;
+};
+smallimg[1].onclick = function () {
+  mainimg.src = smallimg[1].src;
+};
+smallimg[2].onclick = function () {
+  mainimg.src = smallimg[2].src;
+};
+smallimg[3].onclick = function () {
+  mainimg.src = smallimg[3].src;
+};
